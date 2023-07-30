a73ee038d466285c46fd1305a8c958094467865d
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-07-30 14:50:10 +0300

    added chat markup

--- src/Components/ChatMessenger/ChatMessenger.tsx
+++ src/Components/ChatMessenger/ChatMessenger.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import { UserRole } from '../../type/roles';
+import './chatMessenger.scss';
 
 interface Props {
   username: string;
   role: UserRole;
 }
 
-export const ChatMessenger: React.FC<Props> = () => {
+export const ChatMessenger: React.FC<Props> = ({ username }) => {
   return (
     <div>
-      Hello world
+      <div className="chat__container">
+        <div className="chat__body" id="message-container">
+          <div
+            className="chat__body__message"
+          >
+          </div>
+        </div>
+        <div className="chat__footer">
+          <label htmlFor="recipient-input">
+            {`Welcome: ${username}`}
+          </label>
+          <input
+            type="text"
+            id="recipient-input"
+            placeholder="Recipient Name"
+            className="chat__footer__field"
+          />
+          <input
+            type="text"
+            placeholder="Type your message..."
+            className="chat__footer__field"
+          />
+          <button
+            type="button"
+            className="chat__footer__field chat__footer__field--button"
+          >
+            Send
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
