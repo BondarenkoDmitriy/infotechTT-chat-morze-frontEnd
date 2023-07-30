ecff5f34355dc3c1104ffc5e11bb48d6b1e4d3a8
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-07-30 14:23:05 +0300

    added login markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import { Authorization } from './Components/Authorization/Authorization';
 
 export const App: React.FC = () => {
   return (
     <div>
-      Hello world
+      <Authorization />
     </div>
   );
 };
